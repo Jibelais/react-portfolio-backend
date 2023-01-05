e5ccd2b95696c1b6f98ec27c4f88c691792c4cee
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-01-05 10:22:53 -0600

    switched to mongoDB from local database

--- server.js
+++ server.js
@@ -2,15 +2,44 @@
 const express = require("express");
 const cors = require("cors");
 
+
+
 // Import JSON files
 const projects = require("./projects.json");
 const about = require("./about.json");
+
 
 // Create our app object
 const app = express();
 
 // set up middleware
 app.use(cors());
+app.use(express.json()) // parse json bodies from request
+app.use(express.urlencoded({ extended: false })); // to use URL encoded 
+
+
+require("dotenv").config(); 
+const {PORT, DATABASE_URL} = process.env
+const mongoose = require("mongoose")
+
+//Database Connection
+mongoose.connect(DATABASE_URL)
+//Connection 
+mongoose.connection
+    .on("open", () => {console.log("You are connected to DB")})
+    .on("close", () => {console.log("You are disconnected")})
+    .on("error", (error)=> {console.log(error)})
+
+// Model 
+const ProjectSchema = new mongoose.Schema({
+    name: String, 
+    live: String, 
+    git: String, 
+    image: String
+})
+const Project = mongoose.model("Project", ProjectSchema)
+
+
 
 //home route for testing our app
 app.get("/", (req, res) => {
@@ -18,9 +47,23 @@ app.get("/", (req, res) => {
 });
 
 // route for retrieving projects
-app.get("/projects", (req, res) => {
+app.get("/projects", async (req, res) => {
   // send projects via JSON
-  res.json(projects);
+  // res.json(projects);
+  try {
+    res.status(200).json(await Project.find({}))
+   } catch (error) {
+    res.status(400).json(error)
+   }
+ })
+
+ // CREATE
+app.post("/projects", async (req, res) => {
+  try {
+    res.status(200).json(await Project.create(req.body));
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 // route for retrieving about info
@@ -30,7 +73,6 @@ app.get("/about", (req, res) => {
 });
 
 //declare a variable for our port number
-const PORT = process.env.PORT || 4000;
 
 // turn on the server listener
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
